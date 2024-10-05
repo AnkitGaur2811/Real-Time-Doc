7937248c974bdbd4a5f7dff7199b211fe78f98e9
Ankit Gaur <iamankitgaur@gmail.com>
2024-10-05 23:56:25 +0530

    Project Setup

---
+++ realDoc/src/main/java/com/ankit/realDoc/RealDocApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.realDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealDocApplication.class, args);
+	}
+
+}
---
+++ realDoc/src/test/java/com/ankit/realDoc/RealDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.realDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
